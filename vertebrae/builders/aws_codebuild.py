0a3c0042654322e0dfa68745cc85ac1c734a638d
Francis Mireault <mireaulf@gmail.com>
2022-10-18 14:19:08 -0400

    Working sample

--- vertebrae/builders/aws_codebuild.py
+++ vertebrae/builders/aws_codebuild.py
@@ -17,18 +17,22 @@ phases:
   build:
     commands:
       - echo Entered the build phase...
-      - echo Build started on `date`
+      - zig build-lib {} -fPIC -dynamic --library c -target {} -femit-bin={}
+      # - echo Build started on `date`
   post_build:
     commands:
       - echo Entered the post_build phase...
       - echo Build completed on `date`
+artifacts:
+  files:
+    - {}
 '''
 
 
 class AwsCodeBuild:
     def __init__(self):
         # self.log = log
-        logging.getLogger('awscodebuild').setLevel(logging.INFO)
+        # logging.getLogger('awscodebuild').setLevel(logging.INFO)
         self.client = None
 
     def connect(self):
@@ -44,43 +48,53 @@ class AwsCodeBuild:
                 return None
 
         session = boto3.session.Session(profile_name=load_profile())
-        self.client = session.client(service_name='codebuild', region_name='us-west-1')
-
+        self.client = session.client(service_name='codebuild', region_name='us-east-1')
 
     def create_project(self, account_id: str, dcf_name: str):
         # TODO figure out environment.type
-        dcf_name = dcf_name.replace(".", "_")
-        res = self.client.create_project(
-            name=f'{account_id}_{dcf_name}',
-            source=dict(
-                type="S3",
-                location=f'prelude-account-local/{account_id}/src/{dcf_name}/',
-                buildspec=build_spec_c,
-            ),
-            artifacts=dict(
-                type="S3",
-                location=f'prelude-account-local',
-                path=f'{account_id}/dst/{dcf_name}/',
-                name='/',
-                packaging='NONE',
-            ),
-            environment=dict(
-                type='LINUX_GPU_CONTAINER',
-                image='231489180083.dkr.ecr.us-west-1.amazonaws.com/compile@latest',
-                computeType='BUILD_GENERAL1_SMALL',
-            ),
-            serviceRole='arn:aws:iam::231489180083:role/AWSCodeBuildAdminAccess',
-            timeoutInMinutes=5,
-            queuedTimeoutInMinutes=5,
-        )
-        print(f'create_project:\n{json.dumps(res)}\n')
-        return res.project.name
+        ext = dcf_name[dcf_name.rindex('.')+1:]
+        target = 'x86_64-linux-gnu'
+        out = dcf_name.replace(f'.{ext}', f'-{target[:target.index("-")]}.so')
+        build_spec = build_spec_c.format(dcf_name, target, out, out)
+
+        aws_dcf_name = dcf_name.replace(".", "_")
+        project_name = f'{account_id}_{aws_dcf_name}'
+        try:
+            res = self.client.create_project(
+                name=project_name,
+                source=dict(
+                    type="S3",
+                    location=f'prelude-account-local/{account_id}/src/{aws_dcf_name}/',
+                    buildspec=build_spec,
+                ),
+                artifacts=dict(
+                    type="S3",
+                    location=f'prelude-account-local',
+                    path=f'{account_id}/dst/',
+                    name='/',
+                    packaging='NONE',
+                ),
+                environment=dict(
+                    # type='LINUX_CONTAINER',
+                    type='ARM_CONTAINER',
+                    image='231489180083.dkr.ecr.us-west-1.amazonaws.com/compile:latest',
+                    computeType='BUILD_GENERAL1_SMALL',
+                    imagePullCredentialsType='SERVICE_ROLE'
+                ),
+                serviceRole='arn:aws:iam::231489180083:role/AwsCodeBuildAdminRole',
+                timeoutInMinutes=5,
+                queuedTimeoutInMinutes=5,
+            )
+            print(f'create_project:\n{res}\n')
+            return res['project']['name']
+        except self.client.exceptions.ResourceAlreadyExistsException as e:
+            return project_name
 
     def start_build(self, project_name: str):
         res = self.client.start_build(
             projectName=project_name
         )
-        print(f'start_build:\n{json.dumps(res)}\n')
+        print(f'start_build:\n{res}\n')
 
 
 if __name__ == '__main__':
