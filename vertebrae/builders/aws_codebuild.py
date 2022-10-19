47d1c4aa5245195dfe0f04cad83c33542e574d14
Francis Mireault <mireaulf@gmail.com>
2022-10-19 12:49:55 -0400

    Move stuff around

--- vertebrae/builders/aws_codebuild.py
+++ vertebrae/builders/aws_codebuild.py
@@ -1,3 +1,4 @@
+import logging
 from collections import namedtuple
 from enum import Enum
 
@@ -5,7 +6,8 @@ import boto3
 import time
 from botocore.exceptions import ProfileNotFound
 
-region = 'us-east-1'
+from vertebrae.config import Config
+
 build_spec_template = '''
 version: 0.2
 
@@ -25,6 +27,10 @@ build_commands = dict(
     )
 )
 
+CreateProjectRequest = namedtuple('GetProject', 'project_name source_s3 destination_s3 serviceRole environment')
+StartBuildRequest = namedtuple('StartBuildRequest',
+                               'project_name target source_dir_s3 source_file_s3 artifact_filename_s3 artifact_type')
+
 
 class ArtifactType(str, Enum):
     LIBRARY = 'lib'
@@ -32,26 +38,25 @@ class ArtifactType(str, Enum):
 
 
 class AwsCodeBuild:
-    def __init__(self, s3):
-        # self.log = log
-        # logging.getLogger('awscodebuild').setLevel(logging.INFO)
+    def __init__(self, log):
+        self.log = log
+        logging.getLogger('awscodebuild').setLevel(logging.INFO)
         self.client = None
-        self.s3 = s3
 
     def connect(self):
         """ Establish a connection to AWS """
-        # aws = Config.find('aws')
-        # if aws:
-        def load_profile(profile='default'):
-            try:
-                boto3.session.Session(profile_name=profile)
-                boto3.setup_default_session(profile_name=profile)
-                return profile
-            except ProfileNotFound:
-                return None
+        aws = Config.find('aws')
+        if aws:
+            def load_profile(profile='default'):
+                try:
+                    boto3.session.Session(profile_name=profile)
+                    boto3.setup_default_session(profile_name=profile)
+                    return profile
+                except ProfileNotFound:
+                    return None
 
-        session = boto3.session.Session(profile_name=load_profile())
-        self.client = session.client(service_name='codebuild', region_name=region)
+            session = boto3.session.Session(profile_name=load_profile())
+            self.client = session.client(service_name='codebuild', region_name=Config.find('aws')['region'])
 
     @classmethod
     def __generate_buildspec(cls, dcf_name: str, target: str, out_file: str, artifact_type: ArtifactType):
@@ -60,9 +65,7 @@ class AwsCodeBuild:
         build_spec = build_spec_template.format(build_command, out_file)
         return build_spec
 
-    GetProjectRequest = namedtuple('GetProject', 'project_name source_s3 destination_s3 serviceRole environment')
-
-    def get_project(self, req: GetProjectRequest) -> str:
+    def create_project(self, req: CreateProjectRequest) -> str:
         artifact_path_arr = req.destination_s3.split('/', 1)
         try:
             res = self.client.create_project(
@@ -83,12 +86,9 @@ class AwsCodeBuild:
                 timeoutInMinutes=5,
                 queuedTimeoutInMinutes=5,
             )
-            print(f'create_project:\n{res}\n')
             return res['project']['name']
         except self.client.exceptions.ResourceAlreadyExistsException as e:
             return req.project_name
-
-    StartBuildRequest = namedtuple('StartBuildRequest', 'project_name target source_dir_s3 source_file_s3 artifact_filename_s3 artifact_type')
 
     def start_build(self, req: StartBuildRequest):
         res = self.client.start_build(
@@ -96,13 +96,10 @@ class AwsCodeBuild:
             buildspecOverride=self.__generate_buildspec(req.source_file_s3, req.target, req.artifact_filename_s3, req.artifact_type),
             sourceLocationOverride=req.source_dir_s3,
         )
-        print(f'start_build:\n{res}\n')
         return res['build']['id']
 
     def wait_for_builds(self, build_ids: [str], sleep_between_get: int):
-        print(f'waiting for: {build_ids}')
         res = self.client.batch_get_builds(ids=build_ids)
-        print(f'batch_get_builds:\n{res}\n')
         builds_to_watch = [build for build in res['builds'] if build['id'] in build_ids]
         build_done = [build for build in builds_to_watch if build['buildStatus'] != 'IN_PROGRESS']
         if len(build_done) == len(build_ids):
@@ -122,19 +119,16 @@ def get_s3():
             return None
 
     session = boto3.session.Session(profile_name=load_profile())
-    return session.client(service_name='s3', region_name=region)
+    return session.client(service_name='s3', region_name=Config.find('aws')['region'])
 
 
 def start_build(cb: AwsCodeBuild, s3, account_id: str, project_name: str, bucket: str, dcf_name: str, target: str, artifact_type: ArtifactType):
     ext = '.so' if artifact_type == ArtifactType.BINARY else '.exe'
-    dcf_dir = dcf_name.replace(".", "_")
     out_file = dcf_name.replace('.c', f'-{target[:target.index("-")]}{ext}')
-
-    resp = s3.delete_object(Bucket=bucket, Key=f'{account_id}/dst/{out_file}')
-    print(f'delete_object:\n{resp}\n')
-
+    s3.delete_object(Bucket=bucket, Key=f'{account_id}/dst/{out_file}')
+    dcf_dir = dcf_name.replace(".", "_")
     return cb.start_build(
-        AwsCodeBuild.StartBuildRequest(
+        StartBuildRequest(
             project_name=project_name,
             target=target,
             source_dir_s3=f'{bucket}/{account_id}/src/{dcf_dir}/',
@@ -157,7 +151,7 @@ if __name__ == '__main__':
     dcf = '324829a8-9ba9-4559-9b97-4e4cc0cc3bf4_linux.c'
     target = 'x86_64-linux-gnu'
 
-    project_name = cb.get_project(AwsCodeBuild.GetProjectRequest(
+    project_name = cb.create_project(CreateProjectRequest(
         project_name=account_id,
         source_s3=f'{bucket}/{account_id}/src/',
         destination_s3=f'{bucket}/{account_id}/dst/',
