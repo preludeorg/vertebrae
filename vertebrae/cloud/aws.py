2a83c173453f2c908913cb486e5124feb66dd86e
Alex <43142871+khyberspache@users.noreply.github.com>
2022-11-01 14:38:43 -0400

    Aiobotocore (#32)
    * Making vertebrae use aiobotocore
    
    * adding setup
    
    * tweaks
    
    * less code
    
    * better
    
    * working

--- vertebrae/cloud/aws.py
+++ vertebrae/cloud/aws.py
@@ -1,5 +1,4 @@
-import boto3
-from botocore.exceptions import ProfileNotFound
+from aiobotocore.session import AioSession
 
 from vertebrae.config import Config
 
@@ -8,23 +7,6 @@ class AWS:
 
     @classmethod
     def client(cls, service: str):
-        aws = Config.find('aws')
-        if aws:
-            session = boto3.session.Session(profile_name=cls.load_profile(aws.get('profile')))
-            return session.client(service_name=service, region_name=Config.find('aws')['region'])
-
-    @classmethod
-    def resource(cls, service: str):
-        aws = Config.find('aws')
-        if aws:
-            session = boto3.session.Session(profile_name=cls.load_profile(aws.get('profile')))
-            return session.resource(service_name=service, region_name=Config.find('aws')['region'])
-
-    @staticmethod
-    def load_profile(profile='default'):
-        try:
-            boto3.session.Session(profile_name=profile)
-            boto3.setup_default_session(profile_name=profile)
-            return profile
-        except ProfileNotFound:
-            return None
+        if aws := Config.find('aws'):
+            session = AioSession(profile=aws.get('profile'))
+            return session.create_client(service_name=service, region_name=Config.find('aws')['region'])
--- vertebrae/stores/s3.py
+++ vertebrae/stores/s3.py
@@ -1,5 +1,4 @@
 import asyncio
-import concurrent.futures
 import logging
 import os
 from typing import Optional
@@ -17,15 +16,12 @@ class S3:
         logging.getLogger('s3transfer').setLevel(logging.INFO)
         self.client = None
 
-    async def connect(self):
-        """ Establish a connection to AWS """
-        self.client = AWS.client('s3')
-
     async def exists(self, bucket: str, object: str):
         """ Check if a file exists """
         try:
-            self.client.head_object(Bucket=bucket, Key=object)
-            return True
+            async with AWS.client('s3') as client:
+                await client.head_object(Bucket=bucket, Key=object)
+                return True
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == '404':
                 self.log.warning(f'Missing {object}')
@@ -36,65 +32,64 @@ class S3:
         """ Read file from S3 """
         bucket, key = filename.split('/', 1)
         try:
-            body = self.client.get_object(Bucket=bucket, Key=key)
-            return body['Body'].read()
-        except self.client.exceptions.NoSuchKey:
+            async with AWS.client('s3') as client:
+                body = await client.get_object(Bucket=bucket, Key=key)
+                return body['Body'].read()
+        except await self.client.exceptions.NoSuchKey:
             self.log.error(f'Missing {key}')
         except botocore.exceptions.ClientError:
             self.log.error(f'Missing {key}')
 
-    def download_file(self, filename: str, dst: str):
+    async def download_file(self, filename: str, dst: str):
         bucket, key = filename.split('/', 1)
         try:
-            self.client.download_file(bucket, key, dst)
-        except self.client.exceptions.NoSuchKey:
+            async with AWS.client('s3') as client:
+                await client.download_file(bucket, key, dst)
+        except await self.client.exceptions.NoSuchKey:
             self.log.error(f'Missing {key}')
 
-    def upload_file(self, src: str, filename: str):
+    async def upload_file(self, src: str, filename: str):
         bucket, key = filename.split('/', 1)
         try:
-            self.client.upload_file(src, bucket, key)
+            async with AWS.client('s3') as client:
+                await client.upload_file(src, bucket, key)
         except FileNotFoundError:
             self.log.error(f'Missing {src}')
 
     async def write(self, filename: str, contents: str) -> None:
         """ Write file to S3 """
         bucket, key = filename.split('/', 1)
-        self.client.put_object(Body=contents, Bucket=bucket, Key=key)
+        async with AWS.client('s3') as client:
+            await client.put_object(Body=contents, Bucket=bucket, Key=key)
 
     async def delete(self, filename: str) -> None:
         """ Delete file from S3 """
         bucket, key = filename.split('/', 1)
-        self.client.delete_object(Bucket=bucket, Key=key)
+        async with AWS.client('s3') as client:
+            await client.delete_object(Bucket=bucket, Key=key)
 
     async def walk(self, bucket: str, prefix: str) -> [str]:
         """ Get all files of S3 bucket """
         try:
-            obj_list = self.client.list_objects_v2(Bucket=bucket, Prefix=prefix).get('Contents', [])
-            return [f['Key'] for f in obj_list]
+            async with AWS.client('s3') as client:
+                obj_list = await client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+                return [f['Key'] for f in obj_list.get('Contents', [])]
         except botocore.exceptions.ConnectionClosedError:
             self.log.error('Failed connection to AWS S3')
 
     async def read_all(self, bucket: str, prefix: str) -> [str]:
         """ Read all contents of S3 bucket """
-        def _retrieve(k):
+        async def _retrieve(k):
             try:
-                cfg = self.client.get_object(Bucket=bucket, Key=k)
-                return k, cfg['Body'].read()
+                async with AWS.client('s3') as client:
+                    cfg = await client.get_object(Bucket=bucket, Key=k)
+                    return k, await cfg['Body'].read()
             except Exception:
                 return None
 
         my_files = dict()
         try:
-            files = await self.walk(bucket=bucket, prefix=prefix)
-            if not files:
-                return my_files
-
-            tasks = []
-            executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-            for f in files:
-                tasks.append(asyncio.get_event_loop().run_in_executor(executor, _retrieve, f))
-            completed, pending = await asyncio.wait(tasks)
+            completed, pending = await asyncio.wait([_retrieve(f) for f in await self.walk(bucket=bucket, prefix=prefix)])
             for task in completed:
                 key, contents = task.result()
                 if contents:
@@ -103,12 +98,13 @@ class S3:
         except botocore.exceptions.ConnectionClosedError:
             self.log.error('Failed connection to AWS S3')
 
-    def redirect_url(self, bucket: str, object_name: str, expires_in=60) -> Optional[str]:
+    async def redirect_url(self, bucket: str, object_name: str, expires_in=60) -> Optional[str]:
         """ Generate a time-bound redirect URL to a specific file in a bucket """
         try:
-            return self.client.generate_presigned_url(ClientMethod='get_object',
-                                                      Params=dict(Bucket=bucket, Key=object_name),
-                                                      ExpiresIn=expires_in,
-                                                      HttpMethod='GET')
+            async with AWS.client('s3') as client:
+                return await client.generate_presigned_url(ClientMethod='get_object',
+                                                           Params=dict(Bucket=bucket, Key=object_name),
+                                                           ExpiresIn=expires_in,
+                                                           HttpMethod='GET')
         except BotoCoreError:
             raise FileNotFoundError('Cannot find file. Make sure your requested version is correct.')
