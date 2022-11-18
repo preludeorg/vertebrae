6bb442120bf769bb6f930e4eea8399fea8f008dd
Patrick Flaherty <patrick.flaherty@prelude.org>
2022-11-18 16:10:50 -0500

    Merge Env Vars (#39)
    * Merge Env Vars
    
    * Why line?

--- vertebrae/config.py
+++ vertebrae/config.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import yaml
@@ -27,13 +26,6 @@ class Config:
                 return {k.lower(): lower_keys(v) for k, v in dct.items()}
             return dct
 
-        def dict_merge(dct, merge_dct):
-            for k, v in merge_dct.items():
-                if k in dct and isinstance(dct[k], dict) and isinstance(merge_dct[k], dict):
-                    dict_merge(dct[k], merge_dct[k])
-                else:
-                    dct[k] = merge_dct[k]
-
         def strip_yml(path):
             if path and os.path.isfile(path):
                 with open(path, encoding='utf-8') as seed:
@@ -41,7 +33,9 @@ class Config:
             return ''
 
         env = lower_keys(strip_yml(env))
-        os_keys = [k for k in list(os.environ.keys()) if k.lower() in env.keys()]
-        os_env = {k: json.loads(os.getenv(k)) if isinstance(env[k.lower()], dict) else os.getenv(k) for k in os_keys}
-        dict_merge(env, lower_keys(os_env))
+        for k in os.environ.keys():
+            formated = k.lower().replace("_",".")
+            if formated in env.keys():
+                env[formated] = os.environ[k]
+
         return env
